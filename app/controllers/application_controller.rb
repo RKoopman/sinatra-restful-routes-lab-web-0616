c0c6094f578e8b5fe3cdc3b2a039a447bb4f1179
RKoopman <raoul.koopman@gmail.com>
2016-07-06 15:35:17 -0400

    finally finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,57 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+
+  # new
+  get '/recipes/new' do
+    erb :new
+  end
+
+  # create
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  # index
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  # show
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id]) 
+    erb :show
+  end
+
+  # edit
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  # update 
+  post '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  # delete
+  post '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+  end
+
+
+  
 end
+
+
+
+
+
+
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+pizza = Recipe.create(name: "pizza", ingredients: "bread, cheese, sauce", cook_time: "15 min")
+
+pasta = Recipe.create(name: "pasta", ingredients: "penne, sauce, cheese", cook_time: "40 minutes")
